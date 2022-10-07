ec3ec36d8c9ee678418c31a7c8898e6a30557050
Rajesh <98638681+rajeshkbajaj@users.noreply.github.com>
2022-10-07 19:04:40 +0530

    Add files via upload
    updated file

--- CreateEC2Instance.sh
+++ CreateEC2Instance.sh
@@ -5,10 +5,13 @@ echo $VpcId
 
 ## Create a Subnet from below command:
 
-Subnet1=$(aws ec2 create-subnet --vpc-id $VpcId --cidr-block 10.0.1.0/24 | grep SubnetId | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
+Subnet1=$(aws ec2 create-subnet --vpc-id $VpcId --cidr-block 10.10.1.0/24 | grep SubnetId | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
 echo $Subnet1
 
-Subnet2=$(aws ec2 create-subnet --vpc-id $VpcId --cidr-block 10.0.0.0/24 | grep SubnetId | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
+AvaiZone=$(aws ec2 describe-subnets --subnet-ids $Subnet1 | grep AvailabilityZone | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
+echo $AvaiZone
+
+Subnet2=$(aws ec2 create-subnet --vpc-id $VpcId --cidr-block 10.10.2.0/24 | grep SubnetId | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
 echo $Subnet2
 
 ## Create a Internet Gateway from below command:
@@ -36,7 +39,7 @@ c=$(aws ec2 modify-subnet-attribute --subnet-id $Subnet1 --map-public-ip-on-laun
 # Creating Security Group
 SecGroup=$(aws ec2 create-security-group --group-name test_group --description "Security group for demo" --vpc-id $VpcId | grep GroupId | awk '{print $2}'| sed 's/"//g')
 echo $SecGroup
-aws ec2 authorize-security-group-ingress --group-id $SecGroup --protocol tcp --port 22 --cidr 0.0.0.0/0
+d=$(aws ec2 authorize-security-group-ingress --group-id $SecGroup --protocol tcp --port 22 --cidr 0.0.0.0/0)
 
 aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > ./MyKeyPair.pem
 chmod 400 MyKeyPair.pem
@@ -48,16 +51,31 @@ echo $InsId
 aws ec2 create-tags --resources $InsId --tags Key=Name,Value=MyInstance
 aws ec2 wait instance-running \
     --instance-ids $InsId
+	
+StausId=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=MyInstance" --query "Reservations[].Instances[].State.Name" | sed 's/"//g' | sed 's/[][]//g')
+echo $StausId
 
 PublicIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=MyInstance" | grep PublicIpAddress | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
 echo $PublicIP
 
+####creating an ELB
+    
+ELBID=$(aws elb create-load-balancer --load-balancer-name my-load-balancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets $Subnet1 --security-groups $SecGroup | grep DNSName | awk '{print $2}' | sed 's/"//g' | sed 's/{}{}//g')
+echo $ELBID
+
+######creating an launch config using EC2 Instance:
+
+LanConfig=$(aws autoscaling create-launch-configuration --launch-configuration-name my-lc-from-instance --instance-id $InsId)
+
+
+###creating a CloudWatch Alarm based on CPU utilization
 aws cloudwatch put-metric-alarm --alarm-name cpu-mon --alarm-description "Test Alarm when CPU exceeds 70 percent" --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 300 --threshold 70 --comparison-operator GreaterThanThreshold  --dimensions "Name=MyInstance,Value=$InsId" --evaluation-periods 2 --alarm-actions arn:aws:sns:us-east-1:247434585159:Default_CloudWatch_Alarms_Topic1 --unit Percent
 
 AState=$(aws cloudwatch describe-alarms --alarm-names cpu-mon | grep StateValue | awk '{print $2}'| sed 's/"//g' | sed 's/,//g')
 echo $AState
 sleep 2
 
+###
 aws cloudwatch set-alarm-state --alarm-name cpu-mon --state-reason "initializing" --state-value ALARM
 sleep 2
 
@@ -71,19 +89,18 @@ if [ "$BState" = "ALARM" ]; then
      else
          echo "Instance has no Alarm"
 fi
-    
-#####creating an ELB
-    
-ELBID=$(aws elb create-load-balancer --load-balancer-name my-load-balancer --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets $Subnet1 --security-groups $SecGroup | grep DNSName | awk '{print $2}' | sed 's/"//g' | sed 's/{}{}//g')
-echo $ELBID
+sleep 2
 
-######creating an launch config using EC2 Instance:
+StausId1=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=MyInstance" --query "Reservations[].Instances[].State.Name" | sed 's/"//g' | sed 's/[][]//g')
+echo $StausId1
 
-LanConfig=$(aws autoscaling create-launch-configuration --launch-configuration-name my-lc-from-instance --instance-id $InsId)
+######creating an Autoscaling group for EC2 Instance:
 
-######creating an launch config using EC2 Instance:
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg --launch-configuration-name my-lc-from-instance --load-balancer-names my-load-balancer --min-size 1 --max-size 2 --vpc-zone-identifier "$Subnet1,$Subnet2"
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name my-asg --availability-zones us-east-1c --launch-configuration-name my-lc-from-instance --load-balancer-names my-load-balancer --min-size 1 --max-size 2 --vpc-zone-identifier "$Subnet1"
+sleep 5
+StausId2=$(aws ec2 describe-instances --filters --query "Reservations[].Instances[].State.Name" | sed 's/"//g' | sed 's/[][]//g')
+echo $StausId2
      
 echo "Ec2 Instance Demo Finished"
 
